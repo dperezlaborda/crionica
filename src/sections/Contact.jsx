9011f6c7cdf04b7e9383e0da2afeb4b89263a598
dolores <dperezlaborda@gmail.com>
2020-10-18 10:47:48 -0300

    se continua con contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,19 +1,22 @@
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import '../style/sections/contact.scss'
 
 const Contact = () => {
     return (
-        <section>
+        <section id="contact">
             <h2>Contacto</h2>
-            <FontAwesomeIcon icon={faEnvelope} />
-            <p>info@example.com</p>
-            <div>
-                <form>
-                    <input/>
-                    <input/>
-                </form>
+            <div className="email">
+                <FontAwesomeIcon className="env-icon" icon={faEnvelope} />
+                <p>info@example.com</p>
             </div>
+            <form>
+                <input placeholder="Tu Nombre"/>
+                <input placeholder="Tu Email"/>
+                <textarea rows="4" placeholder="Escribinos" />
+            </form>
+            <button className="send-bttn">Enviar</button>
         </section>
     )
 }
