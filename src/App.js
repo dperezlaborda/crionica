39fbaf3de33667efd9ebe34d2853f9274368a0a8
dolores <dperezlaborda@gmail.com>
2020-11-02 14:32:10 -0300

    se agrega scroll

--- src/App.js
+++ src/App.js
@@ -8,24 +8,44 @@ import Objectives from './sections/Objectives';
 import Achiev from './sections/Achiev';
 import News from './sections/News'
 import Contact from './sections/Contact';
+import { Element } from 'react-scroll';
+import { scroller } from 'react-scroll';
 import Footer from './sections/Footer';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+
+
+  const scrollType = {
+    duration: 300,
+    delay: 5000,
+    smooth: true,
+    offset: -10
+  }
+
+  const onClick = () =>{
+    scroller.scrollTo("pie", scrollType);
+  }
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Home />
+        <button onClick={() => onClick()}></button>
+        <Intro/>
+        <About/>
         <Switch>
-          <Route exact path="/intro" component={Intro} />
           <Route exact path="/about" component={About} />
           <Route exact path="/objectives" component={Objectives} />
           <Route exact path="/achievments" component={Achiev} />
           <Route exact path="/news" component={News} />
           <Route exact path="/contact" component={Contact} />
         </Switch>
-        <Footer/>
+        <Element name="pie">
+          <Footer/>
+        </Element>
+        
       </div>
     </Router>
   );
